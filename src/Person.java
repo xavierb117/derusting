43c577f4a09a58f7f3df90c4faf33e5a5494d8da
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-12-14 17:15:55 -0800

    Person

---
+++ src/Person.java
@@ -0,0 +1,59 @@
+public class Person {
+  // Declare a public String instance variable for the name of the person
+  // Declare a private int instance variable for the age of the person
+
+
+  // Create a constructor that takes the name and age of the person
+  // and assigns it to the instance variables
+
+
+  // Create a toString method that gives the name and age of the person
+
+
+  // Implement the below public instance method "birthYear"
+  // There should NOT be any print statement in this method.
+  /**
+   * birthYear returns the year the person was born.
+   * 
+   * The birth year is calculated by subtracting the person's age from currentYear
+   * that's passed in as an int. It assumes that the person's birthday has already
+   * passed this year.
+   * 
+   * @param currentYear an int for the current year
+   * @return The year the person was born
+   */
+
+  public static void main(String[] args) {
+    // Create an instance of Person
+
+    // Create another instance of Person with a different name and age and
+    // assign it to a different variable
+
+    // Print the first person
+
+    // Print the second person
+
+    // Get the name of the first person and store it in a local variable
+
+    // Using the birthYear method, get the birth year of the first person
+    // and store it in a local variable. Input the actual current year (e.g. 2025)
+    // as the argument.
+
+    // In a separate statement, print the local variable holding the birth year.
+
+    /**
+     * Terminology!
+     * 
+     * A class is the overall definition, like a blueprint.
+     * An instance is a specific object made according to that definition.
+     * We use "instance" and "object" to mean the same thing.
+     * 
+     * For example, if there is a Person class, we can make an instance of a specific person: Auberon.
+     * 
+     * There can be many instances for the same class. For example: Auberon, Xinting, Baya are all
+     * different instances of the Person class.
+     * 
+     * Each instance has its own instance variables: Auberon's age can be different from Baya's age.
+     */
+  }
+}
