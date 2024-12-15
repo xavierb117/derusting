05aec3096a61f8c7a69af06978e0db8431586c80
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-12-14 17:33:53 -0800

    ArrayPractice

---
+++ src/ArrayPractice.java
@@ -0,0 +1,22 @@
+public class ArrayPractice {
+  public static void main(String[] args) {
+    // Create an array of Strings of size 4
+
+    // Set the value of the array at each index to be a different String
+    // It's OK to do this one-by-one
+
+    // Get the value of the array at index 2
+
+    // Get the length of the array
+
+    // Iterate over the array using a traditional for loop and print out each item
+
+    // Iterate over the array using a for-each loop and print out each item
+
+    /*
+     * Reminder!
+     * 
+     * Arrays start at index 0
+     */
+  }
+}
