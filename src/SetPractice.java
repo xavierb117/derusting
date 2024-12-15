1adc1ef2d1a4a4b548cd368321aa8a481a6d52ac
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-12-14 17:16:56 -0800

    SetPractice

---
+++ src/SetPractice.java
@@ -0,0 +1,29 @@
+public class SetPractice {
+  public static void main(String[] args) {
+    // Create a HashSet of Strings and assign it to a variable of type Set
+
+    // Add 3 elements to the set
+    // (It's OK to do it one-by-one)
+
+    // Check whether the Set contains a given String
+
+    // Remove an element from the Set
+
+    // Get the size of the Set
+
+    // Iterate over the elements of the Set, printing each one on a separate line
+
+    /*
+     * Warning!
+     * 
+     * The iteration order over the items in a HashSet is NOT GUARANTEED.
+     * 
+     * Even running the exact same program multiple times may give different results.
+     * Do not use a HashSet if order is important! You can use a TreeSet if you
+     * want items in sorted order, or an array or List if you want them in a specified
+     * order.
+     * 
+     * Also remember that sets do NOT have duplicates.
+     */
+  }
+}
