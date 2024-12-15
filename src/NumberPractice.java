d4e4e0b397c45a52fe34d9bb506bd4cc0f7ee1a6
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-12-14 17:27:59 -0800

    NumberPractice

---
+++ src/NumberPractice.java
@@ -0,0 +1,23 @@
+public class NumberPractice {
+  public static void main(String args[]) {
+    // Create a float with a negative value and assign it to a variable
+
+    // Create an int with a positive value and assign it to a variable
+
+    // Use the modulo % operator to find the remainder when the int is divided by 3
+
+    // Use the modulo % operator to determine whether the number is even
+    // (A number is even if it has a remainder of zero when divided by 2)
+    // Use an if-else to print "Even" if the number is even and "Odd"
+    // if the number is odd.
+
+    // Divide the number by another number using integer division
+
+    /*
+     * Reminder!
+     * 
+     * When dividing ints, the result is rounded down.
+     */
+
+  }
+}
